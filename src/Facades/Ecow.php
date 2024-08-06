30ea0bedd07448128c9d0733db61fa50a3d99cc7
= <inman.turbo@gmail.com>
2024-08-06 09:35:54 -0400

    when do we get to start writing business logic?

--- src/Facades/Ecow.php
+++ src/Facades/Ecow.php
@@ -5,6 +5,9 @@ namespace Inmanturbo\Ecow\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string modelClass()
+ * @method static Inmanturbo\Ecow\Models\SavedModel newModel()
+ * 
  * @see \Inmanturbo\Ecow\Ecow
  */
 class Ecow extends Facade
