ab50141fa0a0c93d5d815352e40cfb5f1e0c215c
= <inman.turbo@gmail.com>
2024-08-07 17:26:44 -0400

    “ No surplus words or unnecessary actions. ”   — Marcus Aurelius

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,6 +6,6 @@ uses(TestCase::class)->in(__DIR__);
 
 uses()
     ->beforeEach(function () {
-        $this->artisan('ecow:migrate')->assertExitCode(0);
+        $this->artisan('ecow:migrate', ['--force' => true])->assertExitCode(0);
     })
     ->in('Feature');
